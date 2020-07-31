2449c3c7a2bf1f1194263e0de1ccb0c9ac189de9
Prakshal Jain <prakshalj0512@gmail.com>
2020-07-31 13:33:24 -0700

    qa

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,19 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('CodeCommit Lambda Function at Work to S3! Now, onto CodeDeploy try')
+        'body': json.dumps('This is the qa function. Thanks for visiting.')
     }
 
 
 # aws lambda update-function-code --function-name pjain-func --s3-bucket pjain-git-s3 --s3-key pipe.py --dry-run
+
+# aws deploy create-deployment --application-name pjain-app --deployment-group-name pjain-deploy --revision String 
+
+
+# {
+#             "revisionType": "String",
+#             "string": {
+#               "content": "string",
+#               "sha256": "string"
+#             }
+#           }
