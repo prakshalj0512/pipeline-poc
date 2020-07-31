25155531a9428c5e276d089548b2f528e0d1180d
Prakshal Jain <prakshalj0512@gmail.com>
2020-07-31 14:16:36 -0700

    trigger

--- lambda_function.py
+++ lambda_function.py
@@ -3,8 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the dev function. Thanks for visiting.')
+        'body': json.dumps('This is the dev function. Thanks for visiting. Have a nice day!')
     }
-
-
-# aws lambda update-function-code --function-name pjain-func --s3-bucket pjain-git-s3 --s3-key pipe.py --dry-run
