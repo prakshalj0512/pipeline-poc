0f784368f312012eccadb585fc9684ff58fd9d6e
Prakshal Jain <prakshalj0512@gmail.com>
2020-07-31 13:34:23 -0700

    dev

--- lambda_function.py
+++ lambda_function.py
@@ -3,7 +3,7 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('CodeCommit Lambda Function at Work to S3! Now, onto CodeDeploy try')
+        'body': json.dumps('This is the dev function. Thanks for visiting.')
     }
 
 
