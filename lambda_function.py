0a1e55797065f409bc55d47e4af078efc0f8f59d
Prakshal Jain <prakshalj0512@gmail.com>
2020-07-30 17:19:38 -0700

    file name change

--- lambda_function.py
+++ lambda_function.py
@@ -5,3 +5,6 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('CodeCommit Lambda Function at Work to S3! Now, onto CodeDeploy try')
     }
+
+
+# aws lambda update-function-code --function-name pjain-func --s3-bucket pjain-git-s3 --s3-key pipe.py --dry-run
