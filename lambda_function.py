f635b0db6b1a5a9199ed0a8629885a716d884b67
Prakshal Jain <prakshalj0512@gmail.com>
2020-07-31 14:16:02 -0700

    [REMOVE] extra line

--- lambda_function.py
+++ lambda_function.py
@@ -5,6 +5,3 @@ def lambda_handler(event, context):
         'statusCode': 200,
         'body': json.dumps('This is the prod function. Thanks for visiting.')
     }
-
-
-# aws lambda update-function-code --function-name pjain-func --s3-bucket pjain-git-s3 --s3-key pipe.py --dry-run
