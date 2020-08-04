2956b973d99743f89124474dfb093734b7bf40e4
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-04 16:39:15 -0700

    abc

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file
 TARGET_VERSION=$(cat response.json | jq -r '.Version')
 mv lambda_function.zip lambda_function_${LAMBDA_FUNCTION_ALIAS}_${TARGET_VERSION}.zip
 
-cat > AppSpec.yaml << EOM
+cat > AppSpec.yml << EOM
 version: 0
 Resources:
 - myLambdaFunction:
