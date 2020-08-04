53aa5ae5959d0f31b134f169c97440a18ae531a9
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-04 14:33:33 -0700

    typo

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-echo $S3_BUCKET
-echo $CODE_DEPLOY_DEPLOYMENT_GROUP
-echo $LAMBDA_FUNCTION_NAME
-echo $CODE_DEPLOY_APP_NAME
-
-aws lambda list-functions
-aws lambda get-alias --function-name $LAMBDA_FUNCTION_NAME --name dev
