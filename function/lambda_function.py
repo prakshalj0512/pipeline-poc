e23fd43565ecd37c02d252bfc8ad33a37cbf097a
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-05 14:06:25 -0700

    bkah

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the dev function renewed. a Thanks for visiting. Have a nice day! Time to go PLEASE CHANGE!')
+        'body': json.dumps('This is the dev function renewed v70000. a Thanks for visiting. Have a nice day! Time to go PLEASE CHANGE!')
     }
