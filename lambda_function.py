20dad6d31b7c8a81bbe922c093fa60c79bdea059
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-05 13:59:41 -0700

    [format update

--- lambda_function.py
+++
@@ -1,7 +0,0 @@
-import json
-
-def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps('This is the dev function. Thanks for visiting. Have a nice day! Time to go FINAL PLEASE!')
-    }
