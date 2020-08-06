5d1f0bdda31b2581a9033a562feb963db612e0b9
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-06 16:06:42 -0700

    did something

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the dev asdfasd function renewed v70000. a Thanks for visiting. Have a nice day! Time to go PLEASE CHANGE!')
+        'body': json.dumps('This is the dev asdfasd function renewed v70003. a Thanks for visiting. Have a nice day! Time to go PLEASE CHANGE!')
     }
