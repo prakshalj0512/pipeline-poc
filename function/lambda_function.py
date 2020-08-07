578e223e573351f0a9aceb3f1519dcd841a26b56
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-07 10:21:04 -0700

    did something new

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the dev asdfasd function renewed v70004. a Thanks for visiting. Have a nice day! Time to go PLEASE CHANGE!')
+        'body': json.dumps('This is the dev asdfasd function renewed v700010. a Thanks for visiting. Have a nice day! Time to go PLEASE CHANGE!')
     }
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,33 +7,19 @@ zip -rj lambda_function.zip function/*
 # aws lambda update-function-code --function-name $LAMBDA_FUNCTION_NAME --zip-file fileb://lambda_function.zip --publish > response.json
 # TARGET_VERSION=$(cat response.json | jq -r '.Version')
 UNIQUE_ID=$(openssl rand -base64 12)
-mv lambda_function.zip ${LAMBDA_FUNCTION_NAME}_${UNIQUE_ID}.zip
-
-# cat > AppSpec.yml << EOM
-# version: 0
-# Resources:
-# - myLambdaFunction:
-#     Type: AWS::Lambda::Function
-#     Properties:
-#       Name: $LAMBDA_FUNCTION_NAME
-#       Alias: $LAMBDA_FUNCTION_ALIAS
-#       CurrentVersion: $CURRENT_LAMBDA_VERSION
-#       TargetVersion: $TARGET_VERSION
-# EOM
+# mv lambda_function.zip ${LAMBDA_FUNCTION_NAME}_${UNIQUE_ID}.zip
 
 cat > template.yaml << EOM
 AWSTemplateFormatVersion: '2010-09-09'
 Transform: AWS::Serverless-2016-10-31
-Description: My Lambda function
 Resources:
   LambdaFunction:
     Type: AWS::Serverless::Function
     Properties:
       FunctionName: ${LAMBDA_FUNCTION_NAME}
-      Description: My Lambda function
       Handler: lambda_function.lambda_handler
       Runtime: python3.7
-      CodeUri: s3://${S3_BUCKET}/${LAMBDA_FUNCTION_NAME}_${UNIQUE_ID}.zip
+      CodeUri: s3://lambda_function.zip
       AutoPublishAlias: default
       Timeout: 30
       DeploymentPreference:
