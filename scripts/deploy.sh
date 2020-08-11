81a7192c00d70c140c5cd1675ac0ce4b3295cfe1
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-11 10:07:12 -0700

    did something new

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,7 +1,9 @@
 echo $S3_BUCKET
 echo $LAMBDA_FUNCTION_NAME
+echo $LAMBDA_DEPLOYMENT_PREFERENCE
+echo $BRANCH
 
-CURRENT_LAMBDA_FUNCTION_VERSION=$(aws lambda list-versions-by-function --function-name pjain-func-dev --query "Versions[-1].[Version]" | grep -o -E '[0-9]+')
+CURRENT_LAMBDA_FUNCTION_VERSION=$(aws lambda list-versions-by-function --function-name $LAMBDA_FUNCTION_NAME --query "Versions[-1].[Version]" | grep -o -E '[0-9]+')
 ((TARGET_LAMBDA_VERSION=CURRENT_LAMBDA_FUNCTION_VERSION++))
 TARGET_LAMBDA_FUNCTION_CODE="${LAMBDA_FUNCTION_NAME}_v${TARGET_LAMBDA_VERSION}.zip"
 zip -rj ${TARGET_LAMBDA_FUNCTION_CODE} function/*
@@ -22,7 +24,7 @@ Resources:
       Timeout: 30
       DeploymentPreference:
         Enabled: True
-        Type: AllAtOnce
+        Type: ${LAMBDA_DEPLOYMENT_PREFERENCE}
 EOM
 
 cat template.yaml
