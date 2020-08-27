a8ebd00c50b1d727ad6e7d8349f850606aa6d670
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-26 17:19:54 -0700

    func

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the prod function. Thanks for visiting.')
+        'body': json.dumps('This is the PROD2 function. Thanks for visiting.')
     }
