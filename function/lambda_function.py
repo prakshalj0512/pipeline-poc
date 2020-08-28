2282378e2dbaec6d72af252bf62a69c5664a4337
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-28 14:07:06 -0700

    aszdf

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the leggo function. Thanks for visiting.')
+        'body': json.dumps('This is the leggov7 function. Thanks for visiting.')
     }
