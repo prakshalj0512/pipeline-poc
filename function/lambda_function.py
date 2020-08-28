1e2ffab91efdb7965e1979e099a9b9f501c245d7
Prakshal Jain <prakshalj0512@gmail.com>
2020-08-28 15:16:54 -0700

    lets sese

--- function/lambda_function.py
+++ function/lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('This is the v2maybe function. Thanks for visiting.')
+        'body': json.dumps('This is the abacaac function. Thanks for visiting.')
     }
